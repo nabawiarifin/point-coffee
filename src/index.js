6b1ab2037c4eaf894093d235ed537ff6a76830f6
Nabawi Arifin <nabawi.mgmt@gmail.com>
2023-04-09 17:27:25 +0700

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+const contentDiv = document.createElement('div')
+
+document.body.appendChild(contentDiv)
+contentDiv.setAttribute('id', 'content')
+
+const content = document.getElementById('content')
