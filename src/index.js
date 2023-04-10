d3b8405e9e3a5751d36966bacb3eda0032e52d29
Nabawi Arifin <nabawi.mgmt@gmail.com>
2023-04-10 16:00:59 +0700

    Created main header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
 import './style.css';
-const contentDiv = document.createElement('div')
+const header = document.createElement('header')
+const div = document.createElement('div')
+const button = document.querySelectorAll('button')
 
-document.body.appendChild(contentDiv)
-contentDiv.setAttribute('id', 'content')
+document.body.appendChild(header)
+document.body.appendChild(div)
+div.setAttribute('id', 'content')
 
 const content = document.getElementById('content')
+
+const mainHeader = (() => {
+    const header = document.querySelector('header')
+    const headerHTML = elementFromHTML(`
+        <div class="header-container">
+            <div id="image-header"></div>
+            <ul>
+                <li><button class="header-button">Home</button></li>
+                <li><button class="header-button">Menu</button></li>
+                <li><button class="header-button">Contact</button></li>
+            </ul>
+        </div>
+    `)
+    header.appendChild(headerHTML)
+})();
+
+const headerPress = (() => {
+    const button = document.getElementsByClassName('header-button')
+
+    for (let i = 0; i < button.length; i++) {
+        button[i].addEventListener('click', () => button[i].innerHTML)
+    }
+
+})();
+
+function elementFromHTML(html) {
+    const template = document.createElement('template')
+
+    template.innerHTML = html.trim()
+
+    return template.content.firstElementChild
+}
+
+
